82149f00b8baf3901871481a9b531e293576bb75
= <=>
2021-09-07 12:23:57 +0700

    Update server port

--- index.js
+++ index.js
@@ -1,13 +1,22 @@
 const express = require('express')
 const bodyParser = require('body-parser')
 const app = express()
+require('dotenv').config();
 const cors = require('cors')
-app.use(bodyParser.urlencoded({ extended: false }));
-app.use(bodyParser.json());
-app.use(express.static('public'))
+const httpServer = require('http').createServer(app);
+const whitelist = ['http://localhost:3000','http://localhost:5024','http://localhost:3001']
+const options = {
+  cors: {
+    origin: [...whitelist],
+    methods: ["GET", "POST", "PATCH", "DELETE"],
+    allowedHeaders: ["my-custom-header"],
+    credentials: true,
+  }
+}
+const io = require("socket.io")(httpServer, options);
 //routing imports
 const router = require('./route')
-let whitelist = ['http://localhost:3000','http://localhost:5024','http://localhost:3001']
+
 let corsOptions = {
   origin: function (origin, callback) {
     if (whitelist.indexOf(origin) !== -1) {
@@ -17,12 +26,24 @@ let corsOptions = {
     }
   }
 }
-
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(express.static('public'))
 app.use(cors(corsOptions))
 
 router(app,'/api/v1')
 app.get('/api/v1', (req, res) => {
   res.send('Hello World!')
+})
+
+io.on("connection", socket =>{
+  socket.on("join", (data)=>{
+    socket.join(data.roomId)
+  });
+  socket.on("send message", (data)=>{
+    app.get('/api/v1/')
+    io.to(data.roomId).emit("message", data.updated)
+  });
 })
 
 app.get('*', (req, res) => {
@@ -31,11 +52,8 @@ app.get('*', (req, res) => {
     statusCode: 404,
   })
 })
-
-
-
 // end routing
 
-app.listen(process.env.SERVER_PORT, () => {
+httpServer.listen(process.env.Server_PORT, () => {
   console.log(`Server app listening at http://localhost:${process.env.SERVER_PORT}`)
 })
